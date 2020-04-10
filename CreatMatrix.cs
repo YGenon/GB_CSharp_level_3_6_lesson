6c7b9f8e3a732c375ee9c92976a3d0453bdc9df0
YGenon <genonster@gmail.com>
2020-04-11 02:12:59 +0300

    решено

---
+++ CreatMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class CreatMatrix
+    {
+        public long[,] _siz_matrix { get; set; }
+
+        public CreatMatrix(int siz_matrix)
+        {
+
+            Random random_number = new Random();
+
+            long[,] matrix = new long[siz_matrix, siz_matrix];
+
+            Console.WriteLine("\r\n matrix \r\n");
+
+            for (int i = 0; i < siz_matrix; i++)
+            {
+                for (int j = 0; j < siz_matrix; j++)
+                {
+                    Console.Write("{0,4}", matrix[i, j] = random_number.Next(10) + random_number.Next(10));
+                    //matrix[i, j] = random_number.Next(10) + random_number.Next(10);
+                }
+                Console.WriteLine();
+                _siz_matrix = matrix;
+            }
+            //Console.ReadKey(true);
+        }        
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,45 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
+            #region Создаем 2 матрицы
+
+            CreatMatrix matrix1 = new CreatMatrix(10);
+            CreatMatrix matrix2 = new CreatMatrix(10);            
+
+            #endregion
+
+            //Console.ReadKey(true);
+
+            Console.WriteLine("\r\n РЕЗУЛЬТАТ \r\n");             
+
+            Stopwatch SW = new Stopwatch(); // Создаем объект
+            SW.Start(); // Запускаем
+
+            Task task = Task.Run(() => Multiplication_matrix(matrix1._siz_matrix, matrix2._siz_matrix));
+            //Multiplication_matrix(matrix1._siz_matrix, matrix2._siz_matrix);
+
+            SW.Stop();            
+            Console.WriteLine("Прошло : " + Convert.ToString(SW.Elapsed.Milliseconds) + " ms \r\n");
+
+            Console.ReadKey(true);
+
+            #region Метод умножения двух матриц
+
+            long[,] Multiplication_matrix(long[,] N, long[,] L)
+            {
+                long[,] res = new long[N.GetLength(0), N.GetLength(0)];
+                for (int row = 0; row < N.GetLength(0); row++)
+                {
+                    for (int col = 0; col < N.GetLength(1); col++)
+                    {
+                        Console.Write("{0,4}", res[row, col] = N[row, col] * L[row, col]);                        
+                    }
+                    Console.WriteLine();
+                }
+                return res;
+            }
+            #endregion
         }
+
     }
 }
